d98d39f598ccf91c3407af568bb121d54f1e55f5
laurent bernabé <laurent.bernabe@gmail.com>
2023-10-24 00:27:36 +0200

    checking that opponent king is not in check
    when generating exercise

--- src/main/kotlin/logic/Exercise.kt
+++ src/main/kotlin/logic/Exercise.kt
@@ -1,6 +1,11 @@
 package logic
 
 import components.emptyCell
+import io.github.wolfraam.chessgame.ChessGame
+import io.github.wolfraam.chessgame.board.Board
+import io.github.wolfraam.chessgame.move.KingState
+import io.github.wolfraam.chessgame.move.MoveHelper
+import java.lang.StringBuilder
 import kotlin.random.Random
 
 enum class CellFile {
@@ -453,27 +458,80 @@ val expectedCells = mapOf(
     ),
 )
 
+fun ChessGame.isOppositeKingAttacked(): Boolean {
+    val board = Board.fromFen(fen)
+    val moveHelper = MoveHelper(board)
+    return moveHelper.getKingState(board.sideToMove.flip(), false) == KingState.CHECK
+}
 
 class Exercise(val pieces: List<List<Char>>, val expectedCells: List<Cell>, val isWhiteTurn: Boolean)
 
 fun generateExercise(): Exercise {
     val random = Random.Default
     val pieces = MutableList(8) { MutableList(8) { emptyCell } }
+    val weHaveWhite = random.nextBoolean()
+
+    fun completeFen(input: String): String {
+        val lines = input.split('/')
+        val buffer = StringBuilder(input)
+        if (lines.size < 8) {
+            val missingLinesCount = 8 - lines.size
+            for (i in (0 until missingLinesCount)) {
+                buffer.append("/8")
+            }
+        }
+
+        buffer.append(" ${if (weHaveWhite) 'w' else 'b'} - - 0 1")
+
+        return buffer.toString()
+    }
+
+    fun flattenSpaces(input: String): String {
+        val buffer = StringBuilder()
+        var spacesCount = 0
+
+        for (element in input) {
+            val elementIsSpace = element == ' '
+            if (elementIsSpace) {
+                spacesCount++
+            } else {
+                if (spacesCount > 0) buffer.append(spacesCount)
+                spacesCount = 0
+                buffer.append(element)
+            }
+        }
+
+        if (spacesCount > 0) {
+            buffer.append(spacesCount)
+        }
+
+        return buffer.toString()
+    }
+
+    fun checkForOpponentKingCheck(): Boolean {
+        val fen = completeFen(flattenSpaces(pieces.joinToString(separator = "/") { line ->
+            line.joinToString(separator = "")
+        }))
+        val boardLogic = ChessGame(fen)
+
+        return boardLogic.isOppositeKingAttacked()
+    }
 
     fun placePieceRandomly(pieceType: Char) {
         var isDone = false
         do {
             val fileOrdinal = random.nextInt(8)
             val rankOrdinal = random.nextInt(8)
-            val isOccupied = pieces[rankOrdinal][fileOrdinal] != emptyCell
+            val isOccupied = pieces[7 - rankOrdinal][fileOrdinal] != emptyCell
             if (!isOccupied) {
-                pieces[rankOrdinal][fileOrdinal] = pieceType
-                isDone = true
+                pieces[7 - rankOrdinal][fileOrdinal] = pieceType
+                val opponentKingInCheck = checkForOpponentKingCheck()
+                if (!opponentKingInCheck) {
+                    isDone = true
+                }
             }
         } while (!isDone)
     }
-
-    val weHaveWhite = random.nextBoolean()
 
     // placing enemy pawn
     val enemyPawnRank = if (weHaveWhite) CellRank.Rank2 else CellRank.Rank7
@@ -494,7 +552,7 @@ fun generateExercise(): Exercise {
         )
     }
 
-    pieces[enemyPawnCell.rank.ordinal][enemyPawnCell.file.ordinal] = if (weHaveWhite) 'p' else 'P'
+    pieces[7 - enemyPawnCell.rank.ordinal][enemyPawnCell.file.ordinal] = if (weHaveWhite) 'p' else 'P'
 
     // placing enemy king
 
@@ -523,7 +581,7 @@ fun generateExercise(): Exercise {
         }
     }
 
-    pieces[enemyKingRank.ordinal][enemyKingFile.ordinal] = if (weHaveWhite) 'k' else 'K'
+    pieces[7 - enemyKingRank.ordinal][enemyKingFile.ordinal] = if (weHaveWhite) 'k' else 'K'
 
     // placing our king
     placePieceRandomly(if (weHaveWhite) 'K' else 'k')
